9290eda473da414ea594615ee784b74bdbc2900f
LMartic <51736831+LMartic@users.noreply.github.com>
2020-07-01 15:36:10 +0200

    Database & Diagram

--- Moving.Implementation/Validators/CreateCustomerValidator.cs
+++ Moving.Implementation/Validators/CreateCustomerValidator.cs
@@ -14,8 +14,19 @@ namespace Moving.Implementation.Validators
 
         public CreateCustomerValidator(MovingContext context)
         {
-            RuleFor(x => x.FirstName).NotEmpty();
-            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .MinimumLength(3)
+                .WithMessage("Must be more than 3 characters");
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .MinimumLength(3)
+                .WithMessage("Must be more than 3 characters");
+            RuleFor(x=>x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .Must(x => !context.Customers.Any(cust => cust.Email == x))
+                .WithMessage("Email already exist in the database");
         }
     }
 }
